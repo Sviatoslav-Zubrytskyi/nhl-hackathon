55a1e2021e37c4c554c1e18159c0bcf02e4234d7
artygg <artjoms.grisajevs@student.nhlstenden.com>
2023-10-13 20:46:13 +0200

    Add: test player

--- scrits/index.js
+++ scrits/index.js
@@ -1,3 +1,15 @@
 let app = new PIXI.Application({ width: 1920, height: 1080 })
 
 document.body.appendChild(app.view)
+const player = PIXI.Sprite.from('assets/player.png');
+player.anchor.set(0.5);
+
+player.x = app.screen.width / 2;
+player.y = app.screen.height / 2;
+
+app.stage.addChild(player);
+
+app.ticker.add((delta) =>
+{
+    player.rotation += 0.1 * delta;
+});
