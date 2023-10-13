18ffab57be1bd9ba8b8b4c1a3a160cea86a1c95f
0Empty0 <victor.gargat28@gmail.com>
2023-10-13 21:13:15 +0200

    add canvas to web view

--- scrits/index.js
+++ scrits/index.js
@@ -11,6 +11,8 @@ let app = new PIXI.Application({ width: 1920, height: 1080 })
 app.stage.scale.x = SCALE
 app.stage.scale.y = SCALE
 
+document.body.appendChild(app.view)
+
 const player = PIXI.Sprite.from('assets/player.png')
 player.anchor.set(0.5)
 
