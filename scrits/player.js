8fb88431ee059dfb0dc6fc5af250c21cd8c847a6
StefanCazacu1 <stefan.cazacu@student.nhlstenden.com>
2023-10-14 10:50:52 +0200

    Update player.js

--- scrits/player.js
+++ scrits/player.js
@@ -8,8 +8,8 @@ class Player {
 		this.player = PIXI.Sprite.from('assets/char_walk_left.gif')
 
 		this.player.anchor.set(0)
-		this.player.x = 160
-		this.player.y = 160
+		this.player.x = 260
+		this.player.y = 260
 
 		this.sword = PIXI.Sprite.from('assets/sword.png')
 		this.sword.isrotating = false;
