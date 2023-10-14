5182a6f69ef6091ab38f403d12ce4738b579235a
artygg <artjoms.grisajevs@student.nhlstenden.com>
2023-10-14 03:09:39 +0200

    Edit: Enemy as class

--- scrits/enemy.js
+++ scrits/enemy.js
@@ -2,15 +2,75 @@ let enemies = []
 //playerx = 100;
 //playery = 100;
 
-lookangle = 0
-function create_enemy(x, y) {
-	const enemy = PIXI.Sprite.from('assets/enemy.png')
-	enemy.anchor.set(0.5)
-	enemy.x = x
-	enemy.y = y
-	worldContainer.addChild(enemy)
-	enemies.push(enemy)
+class Enemy {
+	constructor(x, y) {
+		//const enemy = PIXI.Sprite.from('assets/enemy.png')
+		this.sprite = PIXI.Sprite.from('assets/enemy.png')
+		this.sprite.anchor.set(0.5)
+		this.sprite.x = x
+		this.sprite.y = y
+		this.lookangle = 0
+		worldContainer.addChild(this.sprite)
+		this.health = 100;
+		enemies.push(this)
+
+	}
+
+	follow_player(player, speed, delta) {
+		let coltrig = false
+		for (const boundary of boundariesList) {
+			if (isColliding(this.sprite, boundary)) {
+				this.sprite.x = this.sprite.previousX
+				this.sprite.y = this.sprite.previousY
+				coltrig = true
+			}
+		}
+		if (!coltrig) {
+			this.sprite.previousX = this.sprite.x
+			this.sprite.previousY = this.sprite.y
+			let dx = player.x - this.sprite.x
+			let dy = player.y - this.sprite.y
+			let angle = Math.atan2(dy, dx)
+			let angleDifference =
+				(Math.abs(Math.abs(this.lookangle) - Math.abs(angle)) * 180) / Math.PI
+
+			const distance = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2))
+
+			if (angleDifference % 360 <= 60 && distance <= 150) {
+				document.getElementById('found').innerHTML =
+					'I CAN SEE YOU!!! - ' + angleDifference + '<br>DIST:' + distance
+				this.lookangle = angle
+				const vx = Math.cos(angle) * speed
+				const vy = Math.sin(angle) * speed
+				if (distance > 0) {
+					this.sprite.x += vx
+					this.sprite.y += vy
+				}
+			} else if (distance <= 150 && distance >= 10) {
+				this.lookangle += 0.1 * delta
+				document.getElementById('found').innerHTML =
+					"I CAN'T SEE YOU!" +
+					angleDifference +
+					'<br>' +
+					(this.lookangle * 180) / Math.PI +
+					'<br>' +
+					(angle * 180) / Math.PI
+			} else {
+				document.getElementById('found').innerHTML =
+					"I CAN'T SEE YOU!" +
+					angleDifference +
+					'<br>' +
+					(enemies[0].rotation * 180) / Math.PI +
+					'<br>' +
+					(angle * 180) / Math.PI
+			}
+		}
+	}
+
+
+
 }
+
 function hasObstacleBetween(startX, startY, endX, endY) {
 	const distance = Math.sqrt(
 		Math.pow(endX - startX, 2) + Math.pow(endY - startY, 2)
@@ -26,60 +86,9 @@ function hasObstacleBetween(startX, startY, endX, endY) {
 	return trigger
 }
 
-function follow_player(enemy, player, speed, delta) {
-	let coltrig = false
-	for (const boundary of boundariesList) {
-		if (isColliding(enemy, boundary)) {
-			enemy.x = enemy.previousX
-			enemy.y = enemy.previousY
-			coltrig = true
-		}
-	}
-	if (!coltrig) {
-		enemy.previousX = enemy.x
-		enemy.previousY = enemy.y
-		let dx = player.x - enemies[0].x
-		let dy = player.y - enemies[0].y
-		let angle = Math.atan2(dy, dx)
-		let angleDifference =
-			(Math.abs(Math.abs(lookangle) - Math.abs(angle)) * 180) / Math.PI
-
-		const distance = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2))
-
-		if (angleDifference % 360 <= 60 && distance <= 150) {
-			document.getElementById('found').innerHTML =
-				'I CAN SEE YOU!!! - ' + angleDifference + '<br>DIST:' + distance
-			lookangle = angle
-			const vx = Math.cos(angle) * speed
-			const vy = Math.sin(angle) * speed
-			if (distance > 0) {
-				enemy.x += vx
-				enemy.y += vy
-			}
-		} else if (distance <= 150) {
-			lookangle += 0.1 * delta
-			document.getElementById('found').innerHTML =
-				"I CAN'T SEE YOU!" +
-				angleDifference +
-				'<br>' +
-				(enemies[0].rotation * 180) / Math.PI +
-				'<br>' +
-				(angle * 180) / Math.PI
-		} else {
-			document.getElementById('found').innerHTML =
-				"I CAN'T SEE YOU!" +
-				angleDifference +
-				'<br>' +
-				(enemies[0].rotation * 180) / Math.PI +
-				'<br>' +
-				(angle * 180) / Math.PI
-		}
-	}
-}
-
+let Enemy1 = new Enemy(100, 100);
 //create_enemy(app.screen.width / 3 + 100, app.screen.height / 3 + 100)
-create_enemy(100, 100)
 
 app.ticker.add(delta => {
-	follow_player(enemies[0], player, 1, delta)
+	enemies[0].follow_player(player, 1, delta)
 })
